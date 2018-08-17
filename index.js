b2fbeb0aed3e5df9c4aad57cfdd93093b032e764
unknown <shubham007tak>
2018-08-17 17:46:52 +0530

    accordion

---
+++ index.js
@@ -0,0 +1,24 @@
+$("document").ready(()=> {
+
+	$("#heading1").click(function(){
+		$("#para1").slideToggle(1000);
+	});
+	
+});
+
+$("document").ready(()=> {
+
+	$("#heading2").click(function(){
+		$("#para2").slideToggle(1000);
+	});
+	
+});
+$("document").ready(()=> {
+
+	$("#heading3").click(function(){
+		$("#para3").slideToggle(1000);
+	});
+	
+});
+
+
